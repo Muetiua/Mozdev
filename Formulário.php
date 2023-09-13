66b1be87669a77def565308f9656750c984ec061
30muetiua <144534240+30muetiua@users.noreply.github.com>
2023-09-13 12:06:03 +0200

    Add files via upload

---
+++ Formulário.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    var_dump($_POST); // Exibe os dados do formulário para depuração
+
+    
+    // Validação básica do endereço de e-mail
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Abra um arquivo de texto para armazenar os e-mails
+        $arquivo = fopen("emails.txt", "a");
+
+        // Escreva o endereço de e-mail no arquivo
+        fwrite($arquivo, $email . "\n");
+
+        // Feche o arquivo
+        fclose($arquivo);
+
+        // Redirecione de volta para a página do formulário ou exiba uma mensagem de sucesso
+        header("Location: formulario.html");
+        exit();
+    } else {
+        // Redirecione de volta para o formulário com uma mensagem de erro
+        header("Location: formulario.html?erro=1");
+        exit();
+    }
+}
+?>
