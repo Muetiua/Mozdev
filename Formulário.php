24d2f52553bca19b78540fade4ec54bd71dcdf2e
30muetiua <144534240+30muetiua@users.noreply.github.com>
2023-09-17 19:39:10 +0200

    Atualizar o Formulário.php

--- Formulário.php
+++ Formulário.php
@@ -1,24 +1,41 @@
 <?php
-// Verifica se o método de requisição é POST
+// Verifica se o formulário foi submetido
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obter o email do formulário
-    $email = $_POST["Formulário.html"];
-
-    // Valide o email (você pode adicionar validações mais complexas aqui)
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Abra ou crie um arquivo para armazenar os emails
-        $arquivo = "emails.txt";
-        $conteudo = file_get_contents($arquivo);
-
-        // Adicione o novo email ao conteúdo existente
-        $conteudo .= $email . "\n";
-
-        // Salve o conteúdo no arquivo
-        file_put_contents($arquivo, $conteudo);
-
-        echo "Email armazenado com sucesso!";
+    // Coleta os dados do formulário
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = $_POST["mensagem"];
+    
+    // Validação dos campos (pode ser mais robusta dependendo das necessidades)
+    if (empty($nome) || empty($email) || empty($mensagem)) {
+        echo "Todos os campos são obrigatórios.";
     } else {
-        echo "Por favor, insira um email válido.";
+        // Endereço de e-mail para onde a mensagem será enviada
+        $destinatario = "jeremiasmuetiua@gmail.com";
+        
+        // Assunto do e-mail
+        $assunto = "Nova mensagem de contato de $nome";
+        
+        // Corpo do e-mail
+        $corpo = "Nome: $nome\n";
+        $corpo .= "E-mail: $email\n";
+        $corpo .= "Mensagem:\n$mensagem";
+        
+        // Cabeçalhos do e-mail
+        $cabecalhos = "From: $email" . "\r\n" .
+                     "Reply-To: $email" . "\r\n" .
+                     "X-Mailer: PHP/" . phpversion();
+        
+        // Envia o e-mail
+        if (mail($destinatario, $assunto, $corpo, $cabecalhos)) {
+            echo "Mensagem enviada com sucesso. Entraremos em contato em breve!";
+        } else {
+            echo "Erro ao enviar a mensagem. Por favor, tente novamente mais tarde.";
+        }
     }
+} else {
+    // Se o formulário não foi submetido, redireciona para a página de contato
+    header("Location: contato.html");
+    exit;
 }
 ?>
